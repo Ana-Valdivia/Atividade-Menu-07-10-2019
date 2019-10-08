12fd1cb6c2423647e293641a1f0a2025dd957f6f
ana-valdivia <ana.raquel1291@hotmail.com>
2019-10-07 22:21:42 -0300

    Adicionar um item

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -3,7 +3,9 @@ package br.com.etechoracio.atividade;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener{
 
@@ -31,6 +33,22 @@ public class MainActivity extends AppCompatActivity implements CustomDialog.Item
 
         getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.idmenu:
+                CustomDialog menud =new CustomDialog(this);
+                menud.show(getFragmentManager(),"Dialogo");
+                insertMode = true;
+                return true;
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
